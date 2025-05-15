fd374d4289b495c7e0f8df49c28c02291b7585ee
Christian Camacho <chris.ca.dev@gmail.com>
2025-05-15 13:06:48 -0700

    init p4

---
+++ src/project_euler/p4.py
@@ -0,0 +1,13 @@
+"""
+Published on Friday, 16th November 2001, 06:00 pm; Solved by 521199; Difficulty rating: 5%
+
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 times 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+def main():
+    print('project euler p4')
+
+if __name__ == '__main__':
+    main()
