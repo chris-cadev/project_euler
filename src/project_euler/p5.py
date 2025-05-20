97160b0ba16dcf4cc88f12be6bfd6f98a8e2c11b
Christian Camacho <chris.ca.dev@gmail.com>
2025-05-20 11:41:20 -0700

    prepare problem 5

---
+++ src/project_euler/p5.py
@@ -0,0 +1,6 @@
+"""
+Published on Friday, 30th November 2001, 06:00 pm; Solved by 523771; Difficulty rating: 5%
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible divisible with no remainder by all of the numbers from 1 to 20?
+"""
